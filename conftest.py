0791c5b6f615bd2a93f8b39d410c02a70215592d
Viktor Melika <pasola.vik@gmail.com>
2025-01-21 15:00:47 +0200

    21.01 - Last update

--- conftest.py
+++ conftest.py
@@ -1,7 +1,28 @@
+import glob
+import os
+from datetime import datetime
+
 import pytest
 from config import BASE_URL
 from pages.home_page import HomePage
 
+# Auto adding date and time to report name
+def pytest_configure(config):
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    report_path = f"./report/report_{timestamp}.html"
+
+    config.option.htmlpath = report_path
+
+# Auto deletion of old reports
+def pytest_sessionstart(session):
+    report_dir = "./report/"
+    if not os.path.exists(report_dir):
+        os.makedirs(report_dir)
+
+    # Save only last 5 reports
+    reports = sorted(glob.glob(f"{report_dir}/report_*.html"))
+    for report in reports[:-5]:
+        os.remove(report)
 
 @pytest.fixture
 # Open Home page without Newsletter popup
